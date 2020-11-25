438001bfcc9b06fcf1353aaf65ab825cb6dc1e55
Jake Kaad <jkaad@focusvision.com>
2020-11-25 10:09:42 -0800

    Use sprockets

--- lib/emblem/rails/engine.rb
+++ lib/emblem/rails/engine.rb
@@ -4,7 +4,7 @@ module Emblem
   module Rails
     class Engine < ::Rails::Engine
       initializer "ember_rails.setup", :after => :append_assets_path, :group => :all do |app|
-        app.assets.register_engine '.emblem', Emblem::Rails::Template
+        Sprockets.register_engine '.emblem', Emblem::Rails::Template
       end
     end
   end
